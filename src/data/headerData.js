f6259caa1686825e203d6fa70509facba8f9b8d8
Yash Desai <97239801+yashdesai28@users.noreply.github.com>
2025-10-28 22:14:22 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,5 @@ export const headerData = {
     title: "Hello! I am",
     desciption: "Hello! I am Yash Desai, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: '',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: 'banavanu'
 }
